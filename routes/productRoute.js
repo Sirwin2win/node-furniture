363b40f0900dd8a0d79ca99b07b41cd54cc926e1
Sirwin <sirwin2win@gmail.com>
2025-03-17 10:33:33 +0100

    Second asjustment

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -11,27 +11,28 @@ const {
 const multer  = require('multer')
 // const upload = multer({ dest: 'uploads/' })
 
+// const storage = multer.diskStorage({
+//     destination:function(req,file,callback){
+//         const dir='./uploads';
+//         if(!fs.existsSync(dir)){
+//             fs.mkdirSync(dir);
+//         }
+//         callback(null,dir);
+//     },
+//     filename:function(req,file,callback){
+//         callback(null,file.originalname);
+//     }
+// });
+
+
+
 const storage = multer.diskStorage({
-    destination:function(req,file,callback){
-        const dir='./uploads';
-        if(!fs.existsSync(dir)){
-            fs.mkdirSync(dir);
-        }
-        callback(null,dir);
-    },
-    filename:function(req,file,callback){
-        callback(null,file.originalname);
+    destination: "uploads",
+    filename:(req, file, cb)=>{
+        return cb(null, `${Date.now()}${file.originalname}`)
     }
 });
 const upload = multer({ storage: storage }).array('uploads', 5)
-
-
-// const storage = multer.diskStorage({
-//     destination: "uploads",
-//     filename:(req, file, cb)=>{
-//         return cb(null, `${Date.now()}${file.originalname}`)
-//     }
-// });
 // const upload = multer({ storage: storage });
 
 
